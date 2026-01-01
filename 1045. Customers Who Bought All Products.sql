3d84a145cc82eef168669b1e7cd8d09ee177ea79
Zander Wepener <ZanderWepener5@gmail.com>
2026-01-01 22:33:57 +0200

    Create 1045. Customers Who Bought All Products.sql

---
+++ 1045. Customers Who Bought All Products.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+
+SELECT C.customer_id FROM Customer C
+GROUP BY C.customer_id
+HAVING COUNT(DISTINCT C.Product_key) = (SELECT COUNT(P.product_key) FROM Product P)
